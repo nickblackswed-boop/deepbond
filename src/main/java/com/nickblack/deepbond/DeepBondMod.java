94a718ffa85729f58ceff1d4fee4cc31351089ab
nickblackswed-boop <nickblackswed@gmail.com>
2026-08-16 12:35:21 -0700

    Register Deep Bond Ball interaction item

--- src/main/java/com/nickblack/deepbond/DeepBondMod.java
+++ src/main/java/com/nickblack/deepbond/DeepBondMod.java
@@ -12,11 +12,10 @@ public class DeepBondMod implements ModInitializer {
     public static final Item DEEP_BOND_BALL = Registry.register(
             Registries.ITEM,
             Identifier.of(MOD_ID, "deep_bond_ball"),
-            new Item(new Item.Settings().maxCount(1))
+            new DeepBondBallItem(new Item.Settings().maxCount(1))
     );
 
     @Override
     public void onInitialize() {
-        // v0.1 foundation. Bond interaction and progression are added next.
     }
 }
